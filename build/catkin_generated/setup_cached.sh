204a607c704ea60afb17e1284ea40fb3eea696e3
deanzaka <deanzaka.h@gmail.com>
2015-04-29 15:53:12 +0700

    make a new workspace for final project

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/deanzaka/Github/skripsi_ws/build/test_results"
+export CMAKE_PREFIX_PATH="/home/deanzaka/Github/skripsi_ws/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/deanzaka/Github/skripsi_ws/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/deanzaka/Github/skripsi_ws/devel/lib:/home/deanzaka/Github/skripsi_ws/devel/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
+export PATH="/home/deanzaka/Github/skripsi_ws/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/deanzaka/Github/skripsi_ws/devel/lib/pkgconfig:/home/deanzaka/Github/skripsi_ws/devel/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/deanzaka/Github/skripsi_ws/build"
+export PYTHONPATH="/home/deanzaka/Github/skripsi_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/deanzaka/Github/skripsi_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/deanzaka/Github/skripsi_ws/src:$ROS_PACKAGE_PATH"
+export ROS_TEST_RESULTS_DIR="/home/deanzaka/Github/skripsi_ws/build/test_results"

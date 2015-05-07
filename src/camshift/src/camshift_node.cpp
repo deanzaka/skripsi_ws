24c50a28b9c3ad0e2c7d03348d22941b269fe26a
deanzaka <deanzaka.h@gmail.com>
2015-05-08 04:11:16 +0700

    adding camshift on one camera

--- src/camshift/src/camshift_node.cpp
+++ src/camshift/src/camshift_node.cpp
@@ -74,39 +74,127 @@ int main (int argc, char** argv)
 
     	//help();
 
+    	int hsize = 16;
+    	float hranges[] = {0,180};
+    	const float* phranges = hranges;
+    	Rect trackWindow;
+    
     	VideoCapture cap1(1); //capture the video from webcam
 	VideoCapture cap2(2); //capture the video from webcam
 
+	namedWindow( "Histogram", 0 );
+    	namedWindow( "CamShift Demo", 0 );
+    	setMouseCallback( "CamShift Demo", onMouse, 0 );
+    	createTrackbar( "Vmin", "CamShift Demo", &vmin, 256, 0 );
+    	createTrackbar( "Vmax", "CamShift Demo", &vmax, 256, 0 );
+    	createTrackbar( "Smin", "CamShift Demo", &smin, 256, 0 );
+
+    	Mat frame, hsv, hue, mask, hist, histimg = Mat::zeros(200, 320, CV_8UC3), backproj;
+    	bool paused = false;
+
     	while(nh.ok()) {
 
-      	    	Mat imgOriginal1;
-	       bool bSuccess1 = cap1.read(imgOriginal1); // read a new frame from video
-	       Mat imgOriginalCopy1; // make copy of imgOriginal
-	       cap1.read(imgOriginalCopy1);
-	       
-	       Mat imgOriginal2;
-	       bool bSuccess2 = cap2.read(imgOriginal2); // read a new frame from video
-	       Mat imgOriginalCopy2; // make copy of imgOriginal
-	       cap2.read(imgOriginalCopy2);
-
-    		cap1.read(imgOriginal1);
-    		cap2.read(imgOriginal2);
-
-    		if (!bSuccess1) //if not success, break loop
-      		{
-      			cout << "\nCannot read a frame from video stream 1\n";
-      			break;
-      		}
-
-        	if (!bSuccess2) //if not success, break loop
+        	if( !paused )
         	{
-            		cout << "\nCannot read a frame from video stream 1\n";
-            		break;
+            		cap1 >> frame;
+            		if( frame.empty() )
+                		break;
         	}
 
-      	    	imshow("Camera 1", imgOriginal1);
-		imshow("Camera 2", imgOriginal2);
+        	frame.copyTo(image);
 
+        	if( !paused )
+        	{
+            		cvtColor(image, hsv, COLOR_BGR2HSV);
+
+            		if( trackObject )
+            		{
+                		int _vmin = vmin, _vmax = vmax;
+
+                		inRange(hsv, Scalar(0, smin, MIN(_vmin,_vmax)),
+                        	Scalar(180, 256, MAX(_vmin, _vmax)), mask);
+                		int ch[] = {0, 0};
+                		hue.create(hsv.size(), hsv.depth());
+                		mixChannels(&hsv, 1, &hue, 1, ch, 1);
+
+                		if( trackObject < 0 )
+                		{
+                    		Mat roi(hue, selection), maskroi(mask, selection);
+		                    calcHist(&roi, 1, 0, maskroi, hist, 1, &hsize, &phranges);
+		                    normalize(hist, hist, 0, 255, CV_MINMAX);
+
+		                    trackWindow = selection;
+		                    trackObject = 1;
+
+		                    histimg = Scalar::all(0);
+		                    int binW = histimg.cols / hsize;
+		                    Mat buf(1, hsize, CV_8UC3);
+                    		for( int i = 0; i < hsize; i++ )
+                        				buf.at<Vec3b>(i) = Vec3b(saturate_cast<uchar>(i*180./hsize), 255, 255);
+                    		cvtColor(buf, buf, CV_HSV2BGR);
+
+		                    for( int i = 0; i < hsize; i++ )
+		                    {
+		                        int val = saturate_cast<int>(hist.at<float>(i)*histimg.rows/255);
+		                        rectangle( histimg, Point(i*binW,histimg.rows),
+		                                   Point((i+1)*binW,histimg.rows - val),
+		                                   Scalar(buf.at<Vec3b>(i)), -1, 8 );
+		                    }
+                		}
+
+                		calcBackProject(&hue, 1, 0, hist, backproj, &phranges);
+                		backproj &= mask;
+                		RotatedRect trackBox = CamShift(backproj, trackWindow,
+                                    	TermCriteria( CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 10, 1 ));
+                		if( trackWindow.area() <= 1 )
+                		{
+		                    int cols = backproj.cols, rows = backproj.rows, r = (MIN(cols, rows) + 5)/6;
+		                    trackWindow = Rect(trackWindow.x - r, trackWindow.y - r,
+                                       	trackWindow.x + r, trackWindow.y + r) &
+                                 				Rect(0, 0, cols, rows);
+                		}
+
+                		if( backprojMode )
+                    			cvtColor( backproj, image, COLOR_GRAY2BGR );
+                		ellipse( image, trackBox, Scalar(0,0,255), 3, CV_AA );
+            		}
+        	}
+        	else if( trackObject < 0 )
+            			paused = false;
+
+        	if( selectObject && selection.width > 0 && selection.height > 0 )
+        	{
+            		Mat roi(image, selection);
+            		bitwise_not(roi, roi);
+        	}
+
+        	imshow( "CamShift Demo", image );
+        	imshow( "Histogram", histimg );
+
+        	char c = (char)waitKey(10);
+        	if( c == 27 )
+            			break;
+        	switch(c)
+        	{
+        		case 'b':
+            				backprojMode = !backprojMode;
+            				break;
+        		case 'c':
+            				trackObject = 0;
+            				histimg = Scalar::all(0);
+            				break;
+        		case 'h':
+            				showHist = !showHist;
+            				if( !showHist ) destroyWindow( "Histogram" );
+            				else namedWindow( "Histogram", 1 );
+            				break;
+        		case 'p':
+            				paused = !paused;
+            				break;
+        		default:
+            		;
+        	}
+        	
 		ros::spinOnce();
 	      if (waitKey(1) == 27) //wait for 'esc' key press for 1ms. If 'esc' key is pressed, break loop
 	      {

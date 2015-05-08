bf028324c4a99d9d57ce5f8b1e80639a26554fa1
deanzaka <deanzaka.h@gmail.com>
2015-05-08 22:21:35 +0700

    make new panel for camera 2

--- src/camshift/src/camshift_node.cpp
+++ src/camshift/src/camshift_node.cpp
@@ -17,13 +17,21 @@ using namespace std;
 
 Mat image1, image2;
 
-bool backprojMode = false;
-bool selectObject = false;
-int trackObject = 0;
-bool showHist = true;
-Point origin;
-Rect selection;
-int vmin = 10, vmax = 256, smin = 30;
+bool backprojMode1 = false;
+bool selectObject1 = false;
+int trackObject1 = 0;
+bool showHist1 = true;
+Point origin1;
+Rect selection1;
+int vmin1 = 10, vmax1 = 256, smin1 = 30;
+
+bool backprojMode2 = false;
+bool selectObject2 = false;
+int trackObject2 = 0;
+bool showHist2 = true;
+Point origin2;
+Rect selection2;
+int vmin2 = 10, vmax2 = 256, smin2 = 30;
 
 static void help()
 {
@@ -39,29 +47,56 @@ static void help()
             "To initialize tracking, select the object with mouse\n";
 }
 
-static void onMouse( int event, int x, int y, int, void* )
+static void onMouse1( int event, int x, int y, int, void* )
 {
-    	if( selectObject )
+    	if( selectObject1 )
     	{
-        	selection.x = MIN(x, origin.x);
-        	selection.y = MIN(y, origin.y);
-        	selection.width = std::abs(x - origin.x);
-        	selection.height = std::abs(y - origin.y);
+        	selection1.x = MIN(x, origin1.x);
+        	selection1.y = MIN(y, origin1.y);
+        	selection1.width = std::abs(x - origin1.x);
+        	selection1.height = std::abs(y - origin1.y);
 
-        	selection &= Rect(0, 0, image1.cols, image1.rows);
+        	selection1 &= Rect(0, 0, image1.cols, image1.rows);
     	}
 
     	switch( event )
     	{
     	case CV_EVENT_LBUTTONDOWN:
-        	origin = Point(x,y);
-        	selection = Rect(x,y,0,0);
-        	selectObject = true;
+        	origin1 = Point(x,y);
+        	selection1 = Rect(x,y,0,0);
+        	selectObject1 = true;
         	break;
     	case CV_EVENT_LBUTTONUP:
-        	selectObject = false;
-        	if( selection.width > 0 && selection.height > 0 )
-            		trackObject = -1;
+        	selectObject1 = false;
+        	if( selection1.width > 0 && selection1.height > 0 )
+            		trackObject1 = -1;
+        	break;
+    	}
+}
+
+static void onMouse2( int event, int x, int y, int, void* )
+{
+    	if( selectObject2 )
+    	{
+        	selection2.x = MIN(x, origin2.x);
+        	selection2.y = MIN(y, origin2.y);
+        	selection2.width = std::abs(x - origin2.x);
+        	selection2.height = std::abs(y - origin2.y);
+
+        	selection2 &= Rect(0, 0, image2.cols, image2.rows);
+    	}
+
+    	switch( event )
+    	{
+    	case CV_EVENT_LBUTTONDOWN:
+        	origin2 = Point(x,y);
+        	selection2 = Rect(x,y,0,0);
+        	selectObject2 = true;
+        	break;
+    	case CV_EVENT_LBUTTONUP:
+        	selectObject2 = false;
+        	if( selection2.width > 0 && selection2.height > 0 )
+            		trackObject2 = -1;
         	break;
     	}
 }
@@ -84,16 +119,21 @@ int main (int argc, char** argv)
 
 	namedWindow( "Histogram 1", 0 );
     	namedWindow( "CamShift 1", 0 );
-    	setMouseCallback( "CamShift 1", onMouse, 0 );
+    	setMouseCallback( "CamShift 1", onMouse1, 0 );
+
+    	namedWindow("CamShift 1", 0);
+    	createTrackbar( "Vmin", "CamShift 1", &vmin1, 256, 0 );
+    	createTrackbar( "Vmax", "CamShift 1", &vmax1, 256, 0 );
+    	createTrackbar( "Smin", "CamShift 1", &smin1, 256, 0 );
     	
     	namedWindow( "Histogram 2", 0 );
     	namedWindow( "CamShift 2", 0 );
-    	setMouseCallback( "CamShift 2", onMouse, 0 );
+    	setMouseCallback( "CamShift 2", onMouse2, 0 );
 
-    	namedWindow("CamShift Panel", 0);
-    	createTrackbar( "Vmin", "CamShift Panel", &vmin, 256, 0 );
-    	createTrackbar( "Vmax", "CamShift Panel", &vmax, 256, 0 );
-    	createTrackbar( "Smin", "CamShift Panel", &smin, 256, 0 );
+    	namedWindow("CamShift 2", 0);
+    	createTrackbar( "Vmin", "CamShift 2", &vmin2, 256, 0 );
+    	createTrackbar( "Vmax", "CamShift 2", &vmax2, 256, 0 );
+    	createTrackbar( "Smin", "CamShift 2", &smin2, 256, 0 );
 
     	Mat frame1, hsv1, hue1, mask1, hist1, histimg1 = Mat::zeros(200, 320, CV_8UC3), backproj1;
     	Mat frame2, hsv2, hue2, mask2, hist2, histimg2 = Mat::zeros(200, 320, CV_8UC3), backproj2;
@@ -112,29 +152,31 @@ int main (int argc, char** argv)
         	frame1.copyTo(image1);
         	frame2.copyTo(image2);
 
+        	//========================================= CAMERA 1 SELECTION ===================================//
+
         	if( !paused )
         	{
             		cvtColor(image1,hsv1, COLOR_BGR2HSV);
-            		cvtColor(image2,hsv2, COLOR_BGR2HSV);
+            		//cvtColor(image2,hsv2, COLOR_BGR2HSV);
 
-            		if( trackObject )
+            		if( trackObject1 )
             		{
-                		int _vmin = vmin, _vmax = vmax;
+                		int _vmin1 = vmin1, _vmax1 = vmax1;
 
-                		inRange(hsv1, Scalar(0, smin, MIN(_vmin,_vmax)),
-                        	Scalar(180, 256, MAX(_vmin, _vmax)), mask1);
+                		inRange(hsv1, Scalar(0, smin1, MIN(_vmin1,_vmax1)),
+                        	Scalar(180, 256, MAX(_vmin1, _vmax1)), mask1);
                 		int ch[] = {0, 0};
                 		hue1.create(hsv1.size(), hsv1.depth());
                 		mixChannels(&hsv1, 1, &hue1, 1, ch, 1);
 
-                		if( trackObject < 0 )
+                		if( trackObject1 < 0 )
                 		{
-                    		Mat roi(hue1, selection), maskroi(mask1, selection);
+                    		Mat roi(hue1, selection1), maskroi(mask1, selection1);
 		                    calcHist(&roi, 1, 0, maskroi, hist1, 1, &hsize, &phranges);
 		                    normalize(hist1, hist1, 0, 255, CV_MINMAX);
 
-		                    trackWindow = selection;
-		                    trackObject = 1;
+		                    trackWindow = selection1;
+		                    trackObject1 = 1;
 
 		                    histimg1 = Scalar::all(0);
 		                    int binW = histimg1.cols / hsize;
@@ -164,19 +206,21 @@ int main (int argc, char** argv)
                                  				Rect(0, 0, cols, rows);
                 		}
 
-                		if( backprojMode )
+                		if( backprojMode1 )
                     			cvtColor( backproj1, image1, COLOR_GRAY2BGR );
                 		ellipse( image1, trackBox, Scalar(0,0,255), 3, CV_AA );
             		}
         	}
-        	else if( trackObject < 0 )
+        	else if( trackObject1 < 0 )
             			paused = false;
 
-        	if( selectObject && selection.width > 0 && selection.height > 0 )
+        	if( selectObject1 && selection1.width > 0 && selection1.height > 0 )
         	{
-            		Mat roi(image1, selection);
+            		Mat roi(image1, selection1);
             		bitwise_not(roi, roi);
         	}
+
+            	//========================================= camera 1 selection ===================================//
 
         	imshow( "CamShift 1", image1 );
         	imshow( "Histogram 1", histimg1 );
@@ -190,15 +234,15 @@ int main (int argc, char** argv)
         	switch(c)
         	{
         		case 'b':
-            				backprojMode = !backprojMode;
+            				backprojMode1 = !backprojMode1;
             				break;
         		case 'c':
-            				trackObject = 0;
+            				trackObject1 = 0;
             				histimg1 = Scalar::all(0);
             				break;
         		case 'h':
-            				showHist = !showHist;
-            				if( !showHist ) destroyWindow( "Histogram 1" );
+            				showHist1 = !showHist1;
+            				if( !showHist1 ) destroyWindow( "Histogram 1" );
             				else namedWindow( "Histogram 1", 1 );
             				break;
         		case 'p':
